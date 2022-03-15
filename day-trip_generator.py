33c7186e565ba52380f06f36c57d0d7d2a8ca108
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-15 14:42:48 -0400

    day-trip_generator.py

--- day-trip_generator.py
+++ day-trip_generator.py
@@ -20,19 +20,24 @@
 
 
 #Creating a list 
+# current issues : double printing of destinations, restaurants, mode of transportation, and entertainments
+#how to gather all selections to print to console as a itenirary 
+#do all functions have a single function?
 
+#  destination section
 
-from http.client import CONTINUE
 import random
+
 
 trip_destinations = ['Venice', 'Greece', 'Belize','Sydney']
 
 def select_random_trip_destination(daytrips):
         #index 0  index 1  index 2 index 3
-    choosen_random_destination = random.choice(daytrips)
-    print(choosen_random_destination) 
-    return choosen_random_destination
+    chosen_random_destination = random.choice(daytrips)
+    print(chosen_random_destination) 
+    return chosen_random_destination
 select_random_trip_destination(trip_destinations)
+
 
 
 print("Do you like your chosen destination?")
@@ -41,49 +46,104 @@ while answer == 'N':
     print(select_random_trip_destination(trip_destinations)) 
     print("Do you like your chosen destination?")
     answer = input('') 
+if answer == 'Y':
+      print( 'Great choice')
+ 
+
+#resturant section
+
+
+import random
+trip_resturants = ['Lisa', 'Ginos', 'Bella','Strasse']
+def select_random_trip_resturant(foods):
+     #index 0  index 1  index 2 index 3
+    chosen_random_resturant = random.choice(foods)
+    print(chosen_random_resturant) 
+    return chosen_random_resturant
+
+select_random_trip_resturant(trip_resturants)
+
+print("Do you like your chosen destination?")
+answer = input('') 
+while answer == 'N':
+    print ('Sorry to hear your random selection wasnt to your liking')
+    print(select_random_trip_resturant(trip_resturants)) 
+    print("Do you like your chosen destination?")
+    answer = input('') 
+    print ('Sorry to hear your selection wasnt to your liking')
     
 else:
-    print('Awesome location for your trip?')
+    print('We will make reservations for your trip')
 
 
-# * fix bug making location print multiples
+#transportation section
+
+import random
+
+trip_transportation = ['Train', 'Car', 'Limo','Lyft']
+
+def select_random_trip_transportation(rides):
+        #index 0  index 1  index 2 index 3
+    chosen_random_transportation = random.choice(rides)
+    print(chosen_random_transportation) 
+    return chosen_random_transportation
 
 
-# import random
+select_random_trip_transportation(trip_transportation)
+
+print("Do you like your chosen destination?")
+answer = input('') 
+while answer == 'N':
+    print ('Sorry to hear your random selection wasnt to your liking')
+    print(select_random_trip_transportation(trip_transportation)) 
+    print("Do you like your chosen destination?")
+    answer = input('') 
+    print ('Sorry to hear your selection wasnt to your liking')
     
-# trip_resturants = ['Lisa', 'Ginos', 'Bella','Strasse']
-
-# def select_random_trip_resturant(foods):
-#             #index 0  index 1  index 2 index 3
-#         choosen_random_resturant = random.choice(foods)
-#         print(choosen_random_resturant) 
-#         return choosen_random_resturant
+else:
+    print('We will make reservations for your trip')
 
 
+#entertainment section
+
+import random
+
+trip_entertainment = ['Opera', 'Tours', 'Museums', 'Concert']
+
+def select_random_entertainment(things_to_do):
+    #index 0 index 1 index 2 index 3
+    chosen_random_entertainment = random.choice(things_to_do)
+    print(chosen_random_entertainment)
+    return chosen_random_entertainment
+
+select_random_entertainment(trip_entertainment)
 
 
-# import random
+print("Do you like your chosen destination?")
+answer = input('') 
+while answer == 'N':
+    print ('Sorry to hear your random selection wasnt to your liking')
+    print(select_random_entertainment(trip_entertainment)) 
+    print("Do you like your chosen destination?")
+    answer = input('') 
+    print ('Sorry to hear your selection wasnt to your liking')
+else:
+    print('We will make reservations for your trip')
+    print('Congratulations your trip is booked lets confirm the details')
 
-# trip_transportation = ['Train', 'Car', 'Limo','Lyft']
-
-# def select_random_trip_transportation(rides):
-#         #index 0  index 1  index 2 index 3
-#     choosen_random_transportation = random.choice(rides)
-#     print(choosen_random_transportation) 
-#     return choosen_random_transportation
 
 
-# select_random_trip_transportation(trip_transportation)
+# #finalize 
 
+# def finalize_transaction(choices_object):
+#     choices_object = {
+#         'destination': chosen_random_destination,
+#         'transportation': chosen_random_transportation,
+#         'restaurant': chosen_random_resturant,
+#         'entertainment': chosen_random_entertainment
+#     }
 
-# import random
+# finalize_transaction(choices_object)
 
-# trip_entertainment = ['Opera', 'Tours', 'Museums', 'Concert']
-
-# def select_random_entertainment(things_to_do):
-#     #index 0 index 1 index 2 index 3
-#     chosen_random_entertainment = random.choice(things_to_do)
-#     print(chosen_random_entertainment)
-#     return chosen_random_entertainment
-
-# select_random_entertainment(trip_entertainment)
+# print('Time to prepare for your trip. Here are the details, You will be headed to (chosen_random_destination') 
+# ('by (chosen_random_transportation), where you will be attending (chosen_random_entertainment) and dining at (chosen_random_resturant)')
